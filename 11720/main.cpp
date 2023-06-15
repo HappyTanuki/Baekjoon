3238092565c0220037dbe2f525a1bfb47bae0d93
Happytanuki <rlaalstn120@icloud.com>
2023-06-15 21:04:07 +0900

    문제번호들 추가 1/3

--- 11720/main.cpp
+++ 11720/main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() {
+	int length = 0, sum = 0;
+	char numbers[101] = { 0, };
 
+	std::cin >> length;
+	std::cin >> numbers;
+
+	for (int i = 0; i < length; i++) {
+		sum += numbers[i] - '0';
+	}
+
+	std::cout << sum;
 }
