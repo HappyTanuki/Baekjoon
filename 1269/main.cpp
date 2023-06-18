19c7ad9e9b7d48314490da0bd997fe4007a59e79
Happytanuki <rlaalstn120@icloud.com>
2023-06-18 22:38:58 +0900

    하남자식 코딩 ㄷㄷ

--- 1269/main.cpp
+++ 1269/main.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include <algorithm>
 
-int binarySearch(int* array, int length, int value) {
-	int end = length, start = 0, mid = 0;
-
-	while (end - start >= 0) {
-		mid = (start + end) / 2;
-
-		if (array[mid] == value) {
-			return mid;
-		}
-		else if (array[mid] > value) {
-			end = mid - 1;
-		}
-		else {
-			start = mid + 1;
-		}
-	}
-	return false;
-}
-
 int main() {
-	int ASize = 0, BSize = 0, input, i = 0;
+	int ASize = 0, BSize = 0, input = 0, i = 0;
 	int BMinusA = 0, AMinusB = 0;
 
 	std::cin >> ASize >> BSize;
 
-	std::unique_ptr<int[]> A = std::make_unique<int[]>(ASize);
-	std::unique_ptr<int[]> B = std::make_unique<int[]>(BSize);
+	int *A = new int[ASize], *B = new int[BSize];
 
 	for (i = 0; i < ASize; i++) {
 		std::cin >> input;
@@ -38,18 +18,18 @@ int main() {
 		B[i] = input;
 	}
 
-	std::sort(A.get(), A.get() + ASize);
-	std::sort(B.get(), B.get() + BSize);
+	std::sort(A, A + ASize);
+	std::sort(B, B + BSize);
 	
 	AMinusB = ASize;
 	for (i = 0; i < BSize; i++) {
-		if (std::binary_search(A.get(), A.get() + ASize, B[i])) {
+		if (std::binary_search(A, A + ASize, B[i])) {
 			AMinusB--;
 		}
 	}
 	BMinusA = BSize;
 	for (i = 0; i < ASize; i++) {
-		if (std::binary_search(B.get(), B.get() + BSize, A[i])) {
+		if (std::binary_search(B, B + BSize, A[i])) {
 			BMinusA--;
 		}
 	}
